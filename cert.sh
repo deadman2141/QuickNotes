5e21524e03cd721e955a115f14a23c18f03d0312
deadman2141 <44319669+deadman2141@users.noreply.github.com>
2022-12-16 13:19:05 -0500

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -1,5 +1,8 @@
 scp /conf/acme/xenorchestra.fullchain root@192.168.10.100:/etc/ssl/xo/xo.crt
 scp /conf/acme/xenorchestra.key root@192.168.10.100:/etc/ssl/xo/xo.key
-Unifi
-graylog
-so
+scp /conf/acme/unificontroller.key root@192.168.10.60:/etc/ssl/private/unificontroller.key
+scp /conf/acme/unificontroller.crt root@192.168.10.60:/etc/ssl/certs/unificontroller.crt
+scp /conf/acme/graylog.crt root@192.168.10.45:/etc/graylog/tls/graylog-crt.pem
+scp /conf/acme/graylog.key root@192.168.10.45:/etc/graylog/tls/graylog-keyt.pem
+scp /conf/acme/so.key root@172.24.164.56:/etc/pki/managerssl.key
+scp /conf/acme/so.fullchain.pem root@172.24.164.56:/etc/pki/managerssl.crt
